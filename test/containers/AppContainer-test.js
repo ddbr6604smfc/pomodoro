341fbfceaaf1a5ef376be280b94ba63023fe6809
ddbr6604smfc <ddbr6604smfc@gmail.com>
2015-10-18 19:23:11 -0500

    fix broken test

--- test/containers/AppContainer-test.js
+++ test/containers/AppContainer-test.js
@@ -5,14 +5,14 @@ import AppContainer from '../../src/containers/AppContainer';
 
 describe('App Container', () => {
   it('should fetch todos after mounting', () => {
-    const props = {
+    const API = {
       getTodos: expect.createSpy().andReturn(Promise.resolve([])),
       addTodo: () => Promise.resolve([]),
       toggleTodo: () => Promise.resolve([]),
       removeTodo: () => Promise.resolve([]),
     };
 
-    TestUtils.renderIntoDocument(<AppContainer { ...props } />);
-    expect(props.getTodos.calls.length).toBe(1);
+    TestUtils.renderIntoDocument(<AppContainer API={API} />);
+    expect(API.getTodos.calls.length).toBe(1);
   });
 });
